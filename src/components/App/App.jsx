2cfc50aca8e44b74c315345b2d87af4548b65284
Michael Owooje <michaelowooje@gmail.com>
2025-05-17 20:48:48 -0400

    created file structure

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "../Header/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <p>This is your first Vite project</p>
+    </>
+  );
+}
+
+export default App;
