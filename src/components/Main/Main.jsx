5b54fa0b4b0722f687e4a6f00754fea0d1ec265d
Michael Owooje <michaelowooje@gmail.com>
2025-05-29 19:30:15 -0400

    added inital preloader

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Main.css";
 import About from "../About/About";
+import Preloader from "../Preloader/Preloader";
 
 function Main() {
   return (
     <main className="main">
+      <Preloader />
       <About />
     </main>
   );
