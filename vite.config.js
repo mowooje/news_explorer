aa912722bbe4bb958009b1b5695795b6600a9a35
Michael Owooje <michaelowooje@gmail.com>
2025-06-15 18:27:35 -0400

    deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/news_explorer/",
   plugins: [react()],
   server: { port: 3000 },
 });
