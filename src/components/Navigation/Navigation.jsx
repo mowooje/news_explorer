d27c6a129478d78fdc83f2ccf8591d6d9cebe16b
Michael Owooje <michaelowooje@gmail.com>
2025-06-24 20:48:25 -0400

    working on 320px mobile screens

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navigation.css";
 import logo from "../../assets/NewsExplorer.svg";
+import menuIcon from "../../assets/menu-icon.svg";
 
 function Navigation({ handleSignInClick }) {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
+
   return (
     <nav className="navigation">
       <div className="navigation__content">
-        <div>
-          <Link to="/">
-            <img className="navigation__logo" src={logo} alt="nav logo" />
-          </Link>
-        </div>
-        <div className="navigation__right">
+        <Link to="/">
+          <img
+            className="navigation__logo"
+            src={logo}
+            alt="NewsExplorer logo"
+          />
+        </Link>
+
+        <button
+          className="navigation__menu-icon"
+          onClick={toggleMenu}
+          aria-label="Open menu"
+        >
+          <img src={menuIcon} alt="menu icon" />
+        </button>
+
+        <div
+          className={`navigation__right ${
+            isMobileMenuOpen ? "navigation__right--open" : ""
+          }`}
+        >
           <Link to="/" className="navigation__home-link">
             Home
           </Link>
