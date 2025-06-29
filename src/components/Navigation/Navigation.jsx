ac63ac7ab1fcdc4e16cf2c53765329b3cc8195d8
Michael Owooje <michaelowooje@gmail.com>
2025-06-29 12:47:11 -0400

    styled menua bar for 320px

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import "./Navigation.css";
 import logo from "../../assets/NewsExplorer.svg";
 import menuIcon from "../../assets/menu-icon.svg";
+import closeButton from "../../assets/close-button.svg";
 
 function Navigation({ handleSignInClick }) {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -23,9 +24,12 @@ function Navigation({ handleSignInClick }) {
         <button
           className="navigation__menu-icon"
           onClick={toggleMenu}
-          aria-label="Open menu"
+          aria-label={isMobileMenuOpen ? "Close menu" : "Open menu"}
         >
-          <img src={menuIcon} alt="menu icon" />
+          <img
+            src={isMobileMenuOpen ? closeButton : menuIcon}
+            alt={isMobileMenuOpen ? "close button" : "menu icon"}
+          />
         </button>
 
         <div
@@ -33,12 +37,28 @@ function Navigation({ handleSignInClick }) {
             isMobileMenuOpen ? "navigation__right--open" : ""
           }`}
         >
-          <Link to="/" className="navigation__home-link">
+          {isMobileMenuOpen && (
+            <Link to="/" className="navigation__overlay-logo">
+              <img
+                className="navigation__logo"
+                src={logo}
+                alt="NewsExplorer logo"
+              />
+            </Link>
+          )}
+          <Link
+            to="/"
+            className="navigation__home-link"
+            onClick={() => setIsMobileMenuOpen(false)}
+          >
             Home
           </Link>
           <button
             className="navigation__sign-in-btn"
-            onClick={handleSignInClick}
+            onClick={() => {
+              handleSignInClick();
+              setIsMobileMenuOpen(false);
+            }}
           >
             Sign in
           </button>
