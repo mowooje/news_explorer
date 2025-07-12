c26ea30e9e16a421b13bbddc4b8afbb128bc633a
Michael Owooje <michaelowooje@gmail.com>
2025-07-12 19:05:11 -0400

    added saved-news

--- src/components/SavedNews/SavedNews.jsx
+++ src/components/SavedNews/SavedNews.jsx
@@ -1,7 +1,23 @@
-import React from "react";
-import "./SavedNews.css";
-import CurrentUserContext from "../../contexts/CurrentUserContext";
+import { useEffect, useState } from "react";
+import { getItems } from "../../utils/api";
 
-function SavedNews() {}
+function SavedNews() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    getItems().then(setArticles);
+  }, []);
+
+  return (
+    <section>
+      <h2>Saved Articles</h2>
+      <ul>
+        {articles.map((article) => (
+          <li key={article._id}>{article.title}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
 
 export default SavedNews;
