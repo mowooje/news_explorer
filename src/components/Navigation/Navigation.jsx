79f19bc70dfcc0edad0d046f023aa211253b291d
Michael Owooje <michaelowooje@gmail.com>
2025-07-11 22:28:11 -0400

    working on sign in modal for 320px

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -8,8 +8,6 @@ import closeButton from "../../assets/close-button.svg";
 function Navigation({ handleSignInClick }) {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-  const toggleMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
-
   return (
     <nav className="navigation">
       <div className="navigation__content">
@@ -23,46 +21,58 @@ function Navigation({ handleSignInClick }) {
 
         <button
           className="navigation__menu-icon"
-          onClick={toggleMenu}
-          aria-label={isMobileMenuOpen ? "Close menu" : "Open menu"}
+          onClick={() => setIsMobileMenuOpen(true)}
+          aria-label="Open menu"
         >
-          <img
-            src={isMobileMenuOpen ? closeButton : menuIcon}
-            alt={isMobileMenuOpen ? "close button" : "menu icon"}
-          />
+          <img src={menuIcon} alt="menu icon" />
         </button>
 
-        <div
-          className={`navigation__right ${
-            isMobileMenuOpen ? "navigation__right--open" : ""
-          }`}
-        >
-          {isMobileMenuOpen && (
-            <Link to="/" className="navigation__overlay-logo">
-              <img
-                className="navigation__logo"
-                src={logo}
-                alt="NewsExplorer logo"
-              />
-            </Link>
-          )}
-          <Link
-            to="/"
-            className="navigation__home-link"
-            onClick={() => setIsMobileMenuOpen(false)}
-          >
+        <div className="navigation__right">
+          <Link to="/" className="navigation__home-link">
             Home
           </Link>
           <button
             className="navigation__sign-in-btn"
-            onClick={() => {
-              handleSignInClick();
-              setIsMobileMenuOpen(false);
-            }}
+            onClick={handleSignInClick}
           >
             Sign in
           </button>
         </div>
+
+        {isMobileMenuOpen && (
+          <div className="navigation__mobile-menu">
+            <Link to="/" onClick={() => setIsMobileMenuOpen(false)}>
+              <img
+                className="navigation__mobile-logo"
+                src={logo}
+                alt="NewsExplorer logo"
+              />
+            </Link>
+            <button
+              className="navigation__close-icon"
+              onClick={() => setIsMobileMenuOpen(false)}
+              aria-label="Close menu"
+            >
+              <img src={closeButton} alt="close icon" />
+            </button>
+            <Link
+              to="/"
+              className="navigation__home-link"
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
+              Home
+            </Link>
+            <button
+              className="navigation__sign-in-btn"
+              onClick={() => {
+                handleSignInClick();
+                setIsMobileMenuOpen(false);
+              }}
+            >
+              Sign in
+            </button>
+          </div>
+        )}
       </div>
     </nav>
   );
